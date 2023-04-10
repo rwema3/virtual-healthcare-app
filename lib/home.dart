537d4ba50fb3a4761b5c516f248dbd4797ba413e
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-04-10 23:24:32 +0200

    minWidths

--- lib/home.dart
+++ lib/home.dart
@@ -37,14 +37,6 @@ class Home extends StatelessWidget {
             ),
 //            RaisedButton(child: Text('Doctor Sign in'),),
 //            RaisedButton(child: Text('Patient Sign in'),)
-            
-                  },
-                  minWidth: 300.0,
-                  height: 42.0,
-                  child: Text(
-                    'Patient sign in',
-                    style: TextStyle(color: Colors.white),
-                  ),
                 ),
               ),
             ),
