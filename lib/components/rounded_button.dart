d7cb495ff26bb7d3b356ba2c152690ba263ea9c7
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-04-20 23:29:33 +0200

    Widget build

--- lib/components/rounded_button.dart
+++ lib/components/rounded_button.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
         color: colour,
         borderRadius: BorderRadius.circular(30.0),
         child: MaterialButton(
